0edb2a45f55a3761ebe88a6508962e4c1d9c2a5a
pyh <you@example.com>
2022-09-28 15:15:07 +0800

    ad 7.2

--- 异步操作/7.2定时器.js
+++ 异步操作/7.2定时器.js
@@ -37,3 +37,29 @@ var timer = setTimeout(function f() {
  * clearTimeout(id1);
  * clearInterval(id2);
  */
+
+// setTimeout(f, 0)实际上意味着，将任务放到浏览器最早可得的空闲时段执行，
+// 所以那些计算量大、耗时长的任务，常常会被放到几个小部分，分别放到setTimeout(f, 0)里面执行
+
+
+/* 写法一会造成浏览器“堵塞”，因为 JavaScript 执行速度远高于 DOM，会造成大量 DOM 操作“堆积”，
+   而写法二就不会，这就是setTimeout(f, 0)的好处。
+var div = document.getElementsByTagName('div')[0];
+
+// 写法一
+for (var i = 0xA00000; i < 0xFFFFFF; i++) {
+  div.style.backgroundColor = '#' + i.toString(16);
+}
+
+// 写法二
+var timer;
+var i=0x100000;
+
+function func() {
+  timer = setTimeout(func, 0);
+  div.style.backgroundColor = '#' + i.toString(16);
+  if (i++ == 0xFFFFFF) clearTimeout(timer);
+}
+
+timer = setTimeout(func, 0);
+*/
